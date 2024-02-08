4ab8b3310f3276dd5c353a6d6dbb5c84fa3ccf3c
Murali <murali.24.4.91@gmail.com>
2024-02-07 22:48:22 -0600

    firebase push notification pushed

--- firebase-push-notification/App.js
+++ firebase-push-notification/App.js
@@ -1,7 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import { useEffect } from 'react';
 
 export default function App() {
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getToken = async() => {
+      const token = await messaging().getToken();
+      console.log(token);
+  }
+
+  useEffect(()=>{
+    requestUserPermission();
+    getToken();
+    messaging().getInitialNotification().then((message)=>{
+      console.log(message);
+    });
+    
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(remoteMessage.data)
+    });
+
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
